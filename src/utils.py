b724127fb91978de4d91c35e744ce8b76490c3eb
bol17 <gatluakbol7@gmail.com>
2026-09-16 21:02:17 +0300

    feat: Add personalized greet function and update main workflow

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,7 @@ def is_even(n):
 def celsius_to_fahrenheit(c):
     """Converts Celsius to Fahrenheit using F = (C * 9/5) + 32."""
     return (c * 9 / 5) + 32
+
+def greet(name):
+    """Returns a personalized greeting string."""
+    return f"Hello, {name}! Welcome to python_lab."
